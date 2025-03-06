d2261402be960fc385b22afffd3150f5ce24ec33
RaghadAlmehmadi <160329803+RaghadAlmehmadi@users.noreply.github.com>
2025-03-06 14:54:22 +0300

    Update Main.kt

--- app/src/main/java/com/example/classtask/Main.kt
+++ app/src/main/java/com/example/classtask/Main.kt
@@ -1,137 +1,53 @@
 package com.example.classtask
+fun main() {
+    // object from class student
+    val student1 = student("Raghad", 23, 100, "A")
+    student1.displayInfo()
+    println("Scholarship Eligible: ${student1.isEligible()}")
 
-import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.Log
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableLongStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.classtask.ui.theme.ClasstaskTheme
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Blue
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+    // class data for Course information
+    val course1 = course(courseName= "Programming", courseCode= "101", credits= 4)
+    //toString()
+    println("Course information: $course1")
+    //copy()
+    val course2 = course1.copy()
+    println("Copied Course: $course2")
+    //hashCode()
+    println("HashCode of course1: ${course1.hashCode()}")
+    //equals()
+    println("course1 equals course2: ${course1 == course2}")
 
-
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            ClasstaskTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
+    //enum class for DepartmentType
+    val department = DepartmentType.COMPUTER_SCIENCE
+    val currentCredits = 12
+    val courseCredits = 5
+    println("Department: $department\nMax Credits: ${department.maxCredits}\n" +
+            "Can register for course? ${department.canRegister(currentCredits, courseCredits)}")
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
 
-    var counter: MutableState<Int> = remember {
-        mutableStateOf(0)
+// class person
+open class person {
+    var name: String
+    var age: Int
+    constructor(name: String, age: Int) {
+        this.name = name
+        this.age = age
     }
-
-    Row(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalAlignment  = Alignment.Top
-    ) {
-
-        Box(
-            modifier = Modifier,
-            contentAlignment = Alignment.Center
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.profile_imag),
-                contentDescription = "Profile Picture",
-
-            )
-
-        }
-
-        Column(
-            modifier = Modifier
-                .fillMaxSize(),
-           horizontalAlignment = Alignment.Start
-
-
-
-        ) {
-            Text(
-                text = "Raghad",
-                modifier = modifier,
-                color = Color.Black,
-                fontSize = 32.sp,
-                textAlign = TextAlign.Justify
-
-            )
-            Text(
-                text = "major: computer since",
-                modifier = modifier,
-                color = Color.Black,
-                fontSize = 22.sp,
-                textAlign = TextAlign.Justify
-
-                )
-            Button(modifier = Modifier,
-                colors = ButtonDefaults.buttonColors(Color.Blue),
-                onClick = {
-                counter.value = if (counter.value == 0) 1 else 0
-            }) {
-                Text(if (counter.value > 0) "Following" else "Follow")
-            }
-            Text(
-                text = "Followers: ${counter.value}",
-                color = Color.Black,
-                fontSize = 18.sp
-            )
-
-        }
+    open fun displayInfo(){
+        println("the name is $name,and age is $age")
     }
-
 }
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ClasstaskTheme {
-        Greeting("Android")
+//interface ScholarshipEligible
+interface ScholarshipEligible{
+    fun isEligible(): Boolean
+}
+//subclass Student
+class student(name: String, age: Int,var studentId: Int , var grade: String ) : person(name, age),ScholarshipEligible{
+    override fun displayInfo() {
+        println("the name is $name,and age is $age, the grade is $grade, and the student ID $studentId ")
+    }
+    override fun isEligible(): Boolean {
+        return grade == "A" || grade == "B"
     }
 }
